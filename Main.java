c1a73c9083c288a80f45d4a4ce5c86a357219849
Dobrynya <118011783+SwexerCyber@users.noreply.github.com>
2022-11-18 18:43:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,11 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-       Menu.menu();
-
+        Menu.menu();
+//        Students student1 = new Students("alex", "pushkin");
+//        Students student2 = new Students("alex", "pushkin");
+//        System.out.println(student1.hashCode());
+//        System.out.println(student2.hashCode());
         // 1. Добавить студента 2. Добавить группу 3. Изменить студента 4. Удалить студента 5. Удалить группу
     }
 }
